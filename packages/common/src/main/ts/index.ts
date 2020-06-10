4930176211862fda9065418764a6d01ee9b7c1ad
Anton Golub <mailbox@antongolub.ru>
2020-06-10 18:46:04 +0300

    chore: add maskerFactory draft

--- packages/common/src/main/ts/index.ts
+++ packages/common/src/main/ts/index.ts
@@ -1,1 +1,22 @@
 export const foo = 'bar'
+
+export const maskerFactory = (...maskers: any[]) => {
+  const context = createMaskerContext()
+
+  maskers.forEach((masker) => context.registry.set(masker.type, masker))
+
+  const self = (target: any) =>
+    [...context.registry.values()].reduce((memo, masker) => {
+      return masker({
+        memo,
+        next: self
+      })
+    }, target)
+
+  return self
+}
+
+export const createMaskerContext = () => ({
+  registry: new Map(),
+  refs: new WeakMap(),
+})
--- packages/common/src/test/ts/index.ts
+++ packages/common/src/test/ts/index.ts
@@ -1,7 +1,20 @@
-import {foo} from '../../main/ts'
+import {foo, maskerFactory} from '../../main/ts'
 
 describe('',() => {
   it('', () => {
     expect(foo).toBe('bar')
+  })
+})
+
+describe('maskerFactory', () => {
+  it('returns masker', () => {
+    const plain = (_target: any) => '***'
+    plain.type = 'plain'
+
+    const masker = maskerFactory(plain)
+    const target = 'foobar'
+    const result = masker(target)
+
+    expect(result).toBe('***')
   })
 })
