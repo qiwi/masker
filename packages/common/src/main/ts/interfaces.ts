55e6c12afb6a56a9e1087f09325cc0a94958fd08
Anton Golub <mailbox@antongolub.ru>
2020-06-10 14:08:30 +0300

    chore(common): add common interfaces frafts

---
+++ packages/common/src/main/ts/interfaces.ts
@@ -0,0 +1,19 @@
+export type IMaskerType = string
+
+export interface IMasker {
+  type: IMaskerType
+  (context: IMaskerContext): Promise<any>
+  sync?: (context: IMaskerContext) => any
+}
+
+export interface IMaskerContext {
+  target: any,
+  next: IMasker
+  registry: IMaskerRegistry
+}
+
+export interface IMaskerRegistry {
+  get(type: IMaskerType): IMasker | undefined
+  add(type: IMaskerType, masker: IMasker): void
+  remove(type: IMaskerType, masker: IMasker): boolean
+}
