39ab7e3bf4afa4d915f538ac46c23b1dc08b6659
Anton Golub <mailbox@antongolub.ru>
2020-06-16 18:36:05 +0300

    refactor(common): separate interfaces

--- packages/common/src/main/ts/index.ts
+++ packages/common/src/main/ts/index.ts
@@ -3,93 +3,33 @@ import {
   isPromiseLike,
   promisify,
   isEqual,
-  generateId,
+  generateId, flattenObject,
 } from './utils'
 
+import {
+  IExecutor,
+  IExecutorSync,
+  IRawContext,
+  IEnrichedContext,
+  IMaskerPipe,
+  IMaskerPipeName,
+  IMaskerPipeSync,
+  IMaskerPipeAsync,
+  IMaskerPipelineNormalized,
+  IMaskerDirective,
+  IMaskerPipeOutput,
+  IMaskerPipeDeclaration,
+  IMaskerRegistry,
+  IMaskerSchema,
+  ISchemaContext
+} from './interfaces'
+
 export const foo = 'bar'
-
-export type IMaskerType = string
-
-export interface IMasker {
-  type: IMaskerType
-  (context: IMaskerContext): Promise<any>
-  sync?: (context: IMaskerContext) => any
-}
-
-export interface IMakerPipeSync {
-  (input: IMaskerPipeInput): IMaskerPipeOutput
-}
-
-export interface IMakerPipeAsync {
-  (input: IMaskerPipeInput): Promise<IMaskerPipeOutput>
-}
-
-export interface IMaskerPipe {
-  name: IMaskerPipeName
-  exec: IMakerPipeAsync
-  execSync: IMakerPipeSync
-}
-
-export type IMaskerDirective = IMaskerPipeName | [IMaskerPipeName, IMaskerOpts]
-
-export interface IMaskerPipeOutput {
-  value: any
-  pipeline?: IMaskerPipeline
-  final?: boolean
-  schema?: IMaskerSchema
-}
-
-export interface IMaskerPipeInput extends IEnrichedContext {
-  value: any
-  pipeline: IMaskerPipeline
-}
-
-export interface IMaskerContext {
-  target: any
-  next: Function
-}
-
-export type IMaskerRegistry = Map<IMaskerPipeName, IMaskerPipe>
-
-export type IContextId = string
-
-export type IMaskerSharedRefs = Map<IContextId, any>
-
-type IRawContext = {
-  value: any
-  context?: IEnrichedContext
-  pipeline?: IMaskerPipeline
-  registry?: IMaskerRegistry
-  refs?: any
-  mode?: IExecutionMode
-  originPipeline?: IMaskerPipeline
-}
-
-type IEnrichedContext = {
-  value: any,
-  id: IContextId
-  registry: IMaskerRegistry
-  refs: any
-  execute: IExecutor
-  mode: IExecutionMode
-  pipeline: IMaskerPipeline
-  originPipeline: IMaskerPipeline
-  context: IEnrichedContext
-  schema?: IMaskerSchema
-}
-
-export interface IExecutor {
-  (context: IRawContext): IMaskerPipeOutput | Promise<IMaskerPipeOutput>
-  sync: IExecutorSync
-  execSync: IExecutorSync
-  exec: IExecutor
-  id?: string
-}
-export type IExecutorSync = (context: IRawContext) => IMaskerPipeOutput
 
 export const execute: IExecutor = (context: IRawContext) => {
   const sharedContext: IEnrichedContext = normalizeContext(context)
   const {pipeline, mode} = sharedContext
+  
   const pipe = getPipe(pipeline[0])
 
   if (!pipe) {
@@ -128,18 +68,13 @@ export const normalizeContext = ({
   registry = new Map(),
   mode = IExecutionMode.ASYNC,
   originPipeline = pipeline,
+  schema,
 }: IRawContext): IEnrichedContext => {
   const id = generateId()
-  const context = {id, value, refs, registry, execute, mode, pipeline, originPipeline} as IEnrichedContext
+  const context = {id, value, refs, registry, execute, mode, pipeline, originPipeline, schema} as IEnrichedContext
   context.context = context
 
   return context
-}
-
-export type ISchemaContext = {
-  before: IMaskerPipeInput,
-  after: IMaskerPipeOutput,
-  pipe: IMaskerPipelineNormalized
 }
 
 const generateSchema = ({before, after, pipe: {masker: {name}}}: ISchemaContext): IMaskerSchema => {
@@ -163,29 +98,6 @@ const getSchemaType = (value: any): string =>
     : typeof value === 'object' && value !== null
       ? 'object'
       : 'unknown'
-
-export type IMaskerSchema = {
-  type: any
-  maskerDirectives?: Array<IMaskerDirective>
-  properties?: Record<string, IMaskerSchema>
-}
-
-export type IMaskerPipeName = string
-
-export type IMaskerPipeRef = IMaskerPipeName | IMaskerPipe
-
-export type IMaskerOpts = Record<string, any>
-
-export type IMaskerPipeRefWithOpts = [IMaskerPipeRef, IMaskerOpts]
-
-export type IMaskerPipeDeclaration = IMaskerPipeRef | IMaskerPipeRefWithOpts
-
-export type IMaskerPipeline = Array<IMaskerPipeDeclaration>
-
-export type IMaskerPipelineNormalized = {
-  masker: IMaskerPipe,
-  opts?: IMaskerOpts
-}
 
 export const getPipe = (pipe: IMaskerPipeDeclaration, registry?: IMaskerRegistry): IMaskerPipelineNormalized | undefined => {
   let masker
@@ -214,8 +126,8 @@ export const getPipe = (pipe: IMaskerPipeDeclaration, registry?: IMaskerRegistry
   }
 }
 
-export const createPipe = (name: IMaskerPipeName, execSync?: IMakerPipeSync, exec?: IMakerPipeAsync): IMaskerPipe => {
-  const _execSync: IMakerPipeSync = execSync || (() => ({value: '****** masker not implemented'}))
+export const createPipe = (name: IMaskerPipeName, execSync?: IMaskerPipeSync, exec?: IMaskerPipeAsync): IMaskerPipe => {
+  const _execSync: IMaskerPipeSync = execSync || (() => ({value: '****** masker not implemented'}))
   const _exec = exec || promisify(_execSync)
 
   return {
--- packages/common/src/main/ts/interfaces.ts
+++ packages/common/src/main/ts/interfaces.ts
@@ -1,3 +1,5 @@
+import {IExecutionMode} from '@qiwi/substrate'
+
 export type IMaskerType = string
 
 export interface IMasker {
@@ -6,13 +8,101 @@ export interface IMasker {
   sync?: (context: IMaskerContext) => any
 }
 
+export interface IMaskerPipeSync {
+  (input: IMaskerPipeInput): IMaskerPipeOutput
+}
+
+export interface IMaskerPipeAsync {
+  (input: IMaskerPipeInput): Promise<IMaskerPipeOutput>
+}
+
+export interface IMaskerPipe {
+  name: IMaskerPipeName
+  exec: IMaskerPipeAsync
+  execSync: IMaskerPipeSync
+}
+
+export type IMaskerDirective = IMaskerPipeName | [IMaskerPipeName, IMaskerOpts]
+
+export interface IMaskerPipeOutput {
+  value: any
+  pipeline?: IMaskerPipeline
+  final?: boolean
+  schema?: IMaskerSchema
+}
+
+export interface IMaskerPipeInput extends IEnrichedContext {
+  value: any
+  pipeline: IMaskerPipeline
+}
+
 export interface IMaskerContext {
-  target: any,
+  target: any
   next: Function
 }
 
-export interface IMaskerRegistry {
-  get(type: IMaskerType): IMasker | undefined
-  add(type: IMaskerType, masker: IMasker): void
-  remove(type: IMaskerType, masker: IMasker): boolean
+export type IMaskerRegistry = Map<IMaskerPipeName, IMaskerPipe>
+
+export type IContextId = string
+
+export type IRawContext = {
+  value: any
+  schema?: IMaskerSchema
+  context?: IEnrichedContext
+  pipeline?: IMaskerPipeline
+  registry?: IMaskerRegistry
+  refs?: any
+  mode?: IExecutionMode
+  originPipeline?: IMaskerPipeline
+}
+
+export type IEnrichedContext = {
+  value: any,
+  id: IContextId
+  registry: IMaskerRegistry
+  refs: any
+  execute: IExecutor
+  mode: IExecutionMode
+  pipeline: IMaskerPipeline
+  originPipeline: IMaskerPipeline
+  context: IEnrichedContext
+  schema?: IMaskerSchema
+}
+
+export interface IExecutor {
+  (context: IRawContext): IMaskerPipeOutput | Promise<IMaskerPipeOutput>
+  sync: IExecutorSync
+  execSync: IExecutorSync
+  exec: IExecutor
+  id?: string
+}
+export type IExecutorSync = (context: IRawContext) => IMaskerPipeOutput
+
+export type ISchemaContext = {
+  before: IMaskerPipeInput,
+  after: IMaskerPipeOutput,
+  pipe: IMaskerPipelineNormalized
+}
+
+export type IMaskerSchema = {
+  type: any
+  maskerDirectives?: Array<IMaskerDirective>
+  properties?: Record<string, IMaskerSchema>
+}
+
+export type IMaskerPipeName = string
+
+export type IMaskerPipeRef = IMaskerPipeName | IMaskerPipe
+
+export type IMaskerOpts = Record<string, any>
+
+export type IMaskerPipeRefWithOpts = [IMaskerPipeRef, IMaskerOpts]
+
+export type IMaskerPipeDeclaration = IMaskerPipeRef | IMaskerPipeRefWithOpts
+
+export type IMaskerPipeline = Array<IMaskerPipeDeclaration>
+
+export type IMaskerPipelineNormalized = {
+  masker: IMaskerPipe,
+  opts?: IMaskerOpts
 }
