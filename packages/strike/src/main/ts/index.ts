2921696b87b82e3e08dce8d08e6b4bc74f904f46
Anton Golub <golub.anton@gmail.com>
2020-07-30 17:28:41 +0400

    feat(strike): apply strike to strings and numbers only

--- packages/strike/src/main/ts/index.ts
+++ packages/strike/src/main/ts/index.ts
@@ -9,6 +9,10 @@ export const name: IMaskerPipeName = 'strike'
 
 export const stub = '*'
 
-export const pipe: IMaskerPipe = createPipe(name, ({value}: IMaskerPipeInput) => ({value: ('' + value).replace(/[^\s\r\n\t]/gi, stub)}))
+export const pipe: IMaskerPipe = createPipe(name, ({value}: IMaskerPipeInput) => ({
+  value: (typeof value === 'string' || typeof value === 'number')
+    ? value.toString().replace(/[^\s\r\n\t]/gi, stub)
+    : value,
+}))
 
 export default pipe
