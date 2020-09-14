adb12cd296e69139d308a1949bc230ecb59e668e
Anton Golub <golub.anton@gmail.com>
2020-09-14 21:01:59 +0300

    feat(split): apply masker to object keys

--- packages/split/src/main/ts/index.ts
+++ packages/split/src/main/ts/index.ts
@@ -4,9 +4,38 @@ import {
   createPipe,
   mapValues,
   defineNonEnum,
+  IMaskerPipeOutput,
 } from '@qiwi/masker-common'
 
 export const appendPath = (chunk: string = '', prev: string = '') => `${prev ? prev + '.' : ''}${chunk}`
+
+export const randomizeKeys = (keys: string[]) => {
+  const counters: Record<string, number> = {}
+  const getCount = (k: string): number => (counters[k] = (counters[k] || 0) + 1)
+
+  return keys.map((k) => {
+    const count = getCount(k)
+
+    return count === 1
+      ? k
+      : `${k}(${count})`
+  })
+}
+
+// const boxValue = (value: any) => ({value})
+
+const unboxValue = ({value}: any) => value
+
+export const assemble = (values: any[], keys: string[], mapped: any) => {
+  const target: Record<string, any> = Array.isArray(mapped) ? [] : {}
+  const value = randomizeKeys(keys).reduce((m, k, i) => {
+    m[k] = values[i]
+    return m
+  }, target)
+
+  return defineNonEnum(value, '_split_', mapped)
+}
+const echo = <T>(v: T): T => v
 
 export const name: IMaskerPipeName = 'split'
 
@@ -16,7 +45,9 @@ export const pipe: IMaskerPipe = createPipe(
     (typeof value === 'object' && value !== null
       ? ((origin) => {
         const mapped = mapValues(origin, (v, k) => execute.execSync({...context, path: appendPath(k, context.path), pipeline: originPipeline, value: v}))
-        const value = defineNonEnum(mapValues(mapped, ({value}) => value), '_split_', mapped)
+        const keys = Object.keys(mapped).map(Array.isArray(mapped) ? echo : (k) => execute.execSync({...context, pipeline: originPipeline, path: undefined, value: k}).value)
+        const values = (Object.values(mapped) as IMaskerPipeOutput[]).map(unboxValue)
+        const value = assemble(values, keys, mapped)
 
         return {value}
       })(value)
@@ -26,12 +57,15 @@ export const pipe: IMaskerPipe = createPipe(
     (typeof value === 'object' && value !== null
       ? (async(origin) => {
         const mapped = mapValues(origin, (v, k) => execute.exec({...context, path: appendPath(k, context.path), pipeline: originPipeline, value: v}))
-        const keys = Object.keys(mapped)
-        const results = await Promise.all(Object.values(mapped))
+        const keys = Array.isArray(mapped)
+          ? Object.keys(mapped)
+          : (await Promise.all(Object.keys(mapped).map((k) => execute.exec({...context, pipeline: originPipeline, path: undefined, value: k})))).map(unboxValue)
+        const boxedValues = await Promise.all(Object.values(mapped))
+        const values = boxedValues.map(unboxValue)
 
-        keys.forEach((key, idx) => (mapped[key] = results[idx]))
+        keys.forEach((key, idx) => (mapped[key] = boxedValues[idx]))
 
-        const value = defineNonEnum(mapValues(mapped, ({value}) => value), '_split_', mapped)
+        const value = assemble(values, keys, mapped)
 
         return {value}
       })(value)
