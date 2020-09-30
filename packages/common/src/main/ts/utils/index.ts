7416312619a6d1eefc6e6ccd15c2be6a6d74509f
Anton Golub <golub.anton@gmail.com>
2020-09-30 20:26:11 +0400

    perf(common): util refactoring

--- packages/common/src/main/ts/utils/index.ts
+++ packages/common/src/main/ts/utils/index.ts
@@ -1,10 +1,37 @@
-// import {get} from 'lodash'
-
 import {ICallable} from '@qiwi/substrate'
 
-import {deepMap} from './deepmap'
+export {flattie as flattenObject} from 'flattie'
 
-export {deepMap, mapValues} from './deepmap'
+export {cloneDeep as clone} from 'lodash'
+
+export const mapValues = (input: any, fn: (input: any, key?: string) => any) => {
+  const result = Array.isArray(input) ? [] : {}
+
+  mapDescriptors(input, result, fn)
+
+  Object.setPrototypeOf(result, Object.getPrototypeOf(input))
+
+  return result
+}
+
+export const mapDescriptors = (input: any, target: any, fn: (input: any, key?: string) => any) => {
+  const descriptors = Object.getOwnPropertyDescriptors(input)
+  const isArr = Array.isArray(input)
+  for (const i in descriptors) {
+    const descriptor = descriptors[i]
+
+    if (isArr && i === 'length') {
+      continue
+    }
+
+    if (Object.prototype.hasOwnProperty.call(descriptor, 'value')) {
+      Object.defineProperty(target, i, {
+        ...descriptor,
+        value: fn(descriptor.value, i),
+      })
+    }
+  }
+}
 
 export const promisify = (fn: Function) => (...args: any[]) => Promise.resolve(fn(...args))
 
@@ -14,43 +41,6 @@ export const isPromiseLike = (target: any): boolean =>
 export const isEqual = (a: any, b: any): boolean => a === b
 
 export const generateId = () => (Math.random() + '').slice(-8)
-
-// https://stackoverflow.com/questions/44134212/best-way-to-flatten-js-object-keys-and-values-to-a-single-depth-array
-export const flattenObject = (src: Record<string, any>, prefix?: string): Record<string, any> => {
-  const target: Record<string, any> = {}
-  const _prefix = prefix ? prefix + '.' : ''
-
-  for (let i in src) {
-    if (!src.hasOwnProperty(i)) continue
-
-    if (typeof src[i] === 'object' && src[i] !== null) {
-      // Recursion on deeper objects
-      Object.assign(target, flattenObject(src[i] as Record<string, any>, _prefix + i))
-    }
-    else {
-      target[_prefix + i] = src[i]
-    }
-  }
-
-  return target
-}
-
-export const unflattenObject = (src: Record<string, any>): Record<string, any> => {
-  const result: Record<string, any> = {}
-  for (let i in src) {
-    if (src.hasOwnProperty(i)) {
-      // Just a complicated regex to only match a single dot in the middle of the string
-      const keys = i.split(/(?<!\.|^)\.(?!\.+|$)/)
-
-      keys.reduce((r, e, j) => {
-        return r[e] || (r[e] = isNaN(Number(keys[j + 1])) ? (keys.length - 1 === j ? src[i] : {}) : [])
-      }, result)
-    }
-  }
-  return result
-}
-
-export const clone = (src: any): any => deepMap(src, (v) => v)
 
 export const ahook = (value: any, fn: ICallable) => isPromiseLike(value)
     ? value.then(fn)
@@ -62,19 +52,3 @@ export const defineNonEnum = <T>(target: T, key: string, value: any): T => Objec
   configurable: true,
   writable: true,
 })
-
-/*export const substitute = (src: any, path: string): any => {
-  const chunks: string[] = path.split('.')
-  const target = getSubstituteBlank(src)
-
-  chunks.forEach((chunk) => {
-    const subs = getSubstituteBlank(get(src, chunk))
-  })
-}*/
-
-/*export const getSubstituteBlank = (src: any) =>
-    typeof src === 'object' && src !== null
-      ? Array.isArray(src)
-        ? []
-        : {}
-    : src*/
