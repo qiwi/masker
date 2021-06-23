aa1cb0790368fd0fa60b04c0ff556758a6766957
Anton Golub <antongolub@antongolub.com>
2021-06-23 22:06:59 +0300

    test(json): add pipeline processing test

--- packages/json/src/main/ts/index.ts
+++ packages/json/src/main/ts/index.ts
@@ -8,25 +8,21 @@ export const name: IMaskerPipeName = 'json'
 const injectValue = (entry: TJsonEntry, {value}: IMaskerPipeOutput): TJsonEntry => ({...entry, value})
 const injectValues = (entries: TJsonEntry[], outputs: IMaskerPipeOutput[]) => entries.map((entry, i) => injectValue(entry, outputs[i]))
 
-export const processSync = ({value, context}: IMaskerPipeInput) => {
+export const processSync = ({value, context, execute, originPipeline}: IMaskerPipeInput) => {
   const entries = extractJsonEntries(value)
-  const values = entries.map((entry) => context.execute({mode: IExecutionMode.SYNC , context, value: entry.value}) as IMaskerPipeOutput)
+  const values = entries.map((entry) => execute({...context, pipeline: originPipeline, mode: IExecutionMode.SYNC, path: undefined, value: entry.value}) as IMaskerPipeOutput)
 
   return populate(value, injectValues(entries, values))
 }
 
-export const processAsync = async({value, context}: IMaskerPipeInput) => {
+export const processAsync = async({value, context, execute, originPipeline}: IMaskerPipeInput) => {
   const entries = extractJsonEntries(value)
-  const values = await Promise.all(entries.map((entry) => context.execute({...context, value: entry.value})))
+  const values = await Promise.all(entries.map((entry) => execute({...context, pipeline: originPipeline, path: undefined, value: entry.value})))
 
   return populate(value, injectValues(entries, values))
 }
 
-export const populate = (origin: string, entries: TJsonEntry[]): string => entries.reduce((m, cur, i) => {
-  if (entries.length === 0) {
-    return origin
-  }
-
+export const populate = (origin: string, entries: TJsonEntry[]): string => entries.length === 0 ? origin : entries.reduce((m, cur, i) => {
   const prev = i === 0 ? {start: 0, end: 0} : entries[i - 1]
   const post = entries[i + 1] ? '' : origin.slice(cur.end)
   const pre = origin.slice(prev.end, cur.start)
