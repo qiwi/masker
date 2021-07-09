a9769dfff941e66b0ee4ef1e979f14ec0b57ac28
Anton Golub <antongolub@antongolub.com>
2021-07-09 19:31:14 +0300

    feat(pan): capture preformatted PANs, handle Number and String objects
    closes #46

--- packages/pan/src/main/ts/index.ts
+++ packages/pan/src/main/ts/index.ts
@@ -8,11 +8,13 @@ import luhn from 'fast-luhn'
 export const name: IMaskerPipeName = 'pan'
 
 export const pipe: IMaskerPipe = createPipe(name, ({value}: IMaskerPipeInput) => ({
-  value: (typeof value === 'string' || typeof value === 'number')
-    ? value.toString().replace(/\d{13,19}/g, v =>
-      luhn(v)
-        ? `${v.slice(0, 4)} **** **** ${v.slice(-4)}`
-        : v)
+  value: (typeof value === 'string' || typeof value === 'number' || value instanceof String || value instanceof Number)
+    ? value.toString().replace(/\d{13,19}|(\d{4}[\s\-]+){3}\d{4}([\s\-]*\d{3})?/g, v => {
+      const _v = v.replace(/[^\d]/g, '')
+      return luhn(_v)
+        ? `${_v.slice(0, 4)} **** **** ${_v.slice(-4)}`
+        : v
+    })
     : value,
 }))
 
